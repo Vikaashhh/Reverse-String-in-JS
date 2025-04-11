b05168f2e0bd9beec413c59308d68e8436baf22f
Vikash Joshi <82951929+Vikaashhh@users.noreply.github.com>
2025-04-11 17:15:58 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function reverseString(str) {
+    // Split the string into an array, reverse the array, then join it back into a string
+    return str.split('').reverse().join('');
+  }
+  console.log(reverseString("Vikash"));
+  
